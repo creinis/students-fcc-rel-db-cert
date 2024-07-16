259656b08959ea929b1907c7c2a3349684fbb06c
creinis <115181527+creinis@users.noreply.github.com>
2024-07-16 14:20:16 +0200

    feat: create sequence for id in courses table

--- students.sql
+++ students.sql
@@ -60,7 +60,15 @@ ALTER TABLE public.courses OWNER TO freecodecamp;
 --
 -- Name: courses_course_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
 --
+CREATE SEQUENCE public.courses_course_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
+ALTER TABLE public.courses_course_id_seq OWNER TO freecodecamp;
 
 --
 -- Name: courses_course_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
