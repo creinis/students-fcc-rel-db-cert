0278334c068da5296a9b396762ba3d99f51dc47b
creinis <115181527+creinis@users.noreply.github.com>
2024-07-16 14:47:57 +0200

    feat: search and print courses from courses table that starts w letters minor than D

--- students_info.sh
+++ students_info.sh
@@ -9,3 +9,6 @@ PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only
 echo -e "\nFirst name, last name, and GPA of students with a 4.0 GPA:"
 echo "$($PSQL "SELECT first_name, last_name, gpa FROM students WHERE gpa = 4.0")"
 
+echo -e "\nAll course names whose first letter is before 'D' in the alphabet:"
+echo "$($PSQL "SELECT course FROM courses WHERE course < 'D'")"
+
