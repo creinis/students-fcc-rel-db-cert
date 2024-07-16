0426976d490c78679a26549119e01500239f60f2
creinis <115181527+creinis@users.noreply.github.com>
2024-07-16 14:52:53 +0200

    feat: courses WHERE course LIKE '_e%' OR course LIKE '%s' ORDER BY course DESC LIMIT 5

--- students_info.sh
+++ students_info.sh
@@ -21,3 +21,6 @@ echo "$($PSQL "SELECT last_name FROM students WHERE last_name ILIKE '%sa%' OR la
 echo -e "\nFirst name, last name, and GPA of students who have not selected a major and either their first name begins with 'D' or they have a GPA greater than 3.0:"
 echo "$($PSQL "SELECT first_name, last_name, gpa FROM students WHERE major_id IS NULL AND (first_name LIKE 'D%' OR gpa > 3.0)")"
 
+echo -e "\nCourse name of the first five courses, in reverse alphabetical order, that have an 'e' as the second letter or end with an 's':"
+echo "$($PSQL "SELECT course FROM courses WHERE course LIKE '_e%' OR course LIKE '%s' ORDER BY course DESC LIMIT 5")"
+
